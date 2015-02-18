7069fe30bea62055d4a7e7d95f892b3dafc4d3a4
Scott Hasbrouck <scott@ginkgotree.com>
2015-02-18 00:48:50 -0500

    Try a different base64 for ie

--- src/branch.js
+++ src/branch.js
@@ -175,55 +175,7 @@ var Branch = function Branch(app_id, debug, callback) {
 
 			//define jsonp request
 			var jsonpRequest = (function(){
-
-				//base64 encoding goes here because... ie9.
-				var base64Encode = function (input) {
-					var utf8Encode = function (string) {
-						string = string.replace(/\r\n/g,"\n");
-						var utftext = "";
-						for (var n = 0; n < string.length; n++) {
-							var c = string.charCodeAt(n);
-							if (c < 128) {
-								utftext += String.fromCharCode(c);
-							}
-							else if((c > 127) && (c < 2048)) {
-								utftext += String.fromCharCode((c >> 6) | 192);
-								utftext += String.fromCharCode((c & 63) | 128);
-							}
-							else {
-								utftext += String.fromCharCode((c >> 12) | 224);
-								utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-								utftext += String.fromCharCode((c & 63) | 128);
-							}
-						}
-						return utftext;
-					};
-
-					var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-					var output = "";
-					var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-					var i = 0;
-					input = utf8Encode(input);
-
-					while (i < input.length) {
-						chr1 = input.charCodeAt(i++);
-						chr2 = input.charCodeAt(i++);
-						chr3 = input.charCodeAt(i++);
-						enc1 = chr1 >> 2;
-						enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-						enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-						enc4 = chr3 & 63;
-						if (isNaN(chr2)) {
-							enc3 = enc4 = 64;
-						} else if (isNaN(chr3)) {
-							enc4 = 64;
-						}
-						output = output +
-						keyStr.charAt(enc1) + keyStr.charAt(enc2) +
-						keyStr.charAt(enc3) + keyStr.charAt(enc4);
-					}
-					return output;
-				};
+				var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
 				var request = {};
 
@@ -231,7 +183,7 @@ var Branch = function Branch(app_id, debug, callback) {
 					var callback_name = options.callbackName || 'callback';
 					var on_success = options.onSuccess || function(){};
 					var on_timeout = options.onTimeout || function(){};
-					var data = encodeURIComponent(base64Encode(JSON.stringify(options.data))) || "";
+					var data = encodeURIComponent(Base64.encode(JSON.stringify(options.data))) || "";
 					var timeout = options.timeout || 10; // sec
 
 					var timeout_trigger = window.setTimeout(function(){
