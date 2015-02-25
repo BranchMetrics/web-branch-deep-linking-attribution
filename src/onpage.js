4eecbe2bd853042f23780af407fc33465129f325
Dmitri Gaskin <dmitrig01@gmail.com>
2015-02-24 17:12:22 -0800

    convert to tabs for consistency

--- src/onpage.js
+++ src/onpage.js
@@ -8,22 +8,22 @@
  */
 
 (function(root, doc, scriptStr, branchStr, createCallback, branchSdk, funcs, i, scriptTag, firstScript) {
-  if (!root[branchStr] || !root[branchStr]._q) {
-    while (i < funcs.length) {
-      createCallback(branchSdk, funcs[i++]);
-    }
+	if (!root[branchStr] || !root[branchStr]._q) {
+		while (i < funcs.length) {
+			createCallback(branchSdk, funcs[i++]);
+		}
 
-    scriptTag = doc.createElement(scriptStr);
-    scriptTag.async = 1;
-    scriptTag.src = "https://s3-us-west-1.amazonaws.com/branch-web-sdk/branch-0.x.min.js";
-    firstScript = doc.getElementsByTagName(scriptStr)[0];
-    firstScript.parentNode.insertBefore(scriptTag, firstScript);
+		scriptTag = doc.createElement(scriptStr);
+		scriptTag.async = 1;
+		scriptTag.src = "https://s3-us-west-1.amazonaws.com/branch-web-sdk/branch-0.x.min.js";
+		firstScript = doc.getElementsByTagName(scriptStr)[0];
+		firstScript.parentNode.insertBefore(scriptTag, firstScript);
 
-    root[branchStr] = branchSdk;
-  }
+		root[branchStr] = branchSdk;
+	}
 })(window, document, "script", 'branch', function(branch, name) {
-  branch[name] = function() {
-    branch._q.push([ name, arguments ]);
-  };
+	branch[name] = function() {
+		branch._q.push([ name, arguments ]);
+	};
 }, { _q: [], _v: 1 }, // _q: the "queue" of calls, _v: the "version" of the embed script
 'init;data;setIdentity;logout;track;link;sendSMS;referrals;credits;redeem;banner'.split(';'), 0);
