0866e3542eddd6a2d637a863b4372d4d4b9ca2a5
Scott Hasbrouck <scott@ginkgotree.com>
2015-03-11 11:28:28 +0800

    First branch test working: track

--- tests/3_branch.js
+++ tests/3_branch.js
@@ -6,16 +6,20 @@ goog.require('storage');
 
 var server;
 
-describe('Resources', function() {
-	var branch = new Branch(), api, requests;
+describe('Branch', function() {
+	var branch = new Branch(), request, requests;
 	before(function() {
-		branch.init(app_id, function(err, data) { });
+		branch.initialized = true;
 	});
 
 	beforeEach(function() {
-		api = sinon.stub(branch._branchAPI, "request", function(resource, obj, storage, callback) {
-			// do whatever here
+		request = request || sinon.stub(branch._branchAPI, "request", function(resource, obj, storage, callback) {
+			request.resource = resource;
+			request.obj = obj;
+			requests.push(request);
+			callback();
 		});
+		requests = [];
 	});
 
 	afterEach(function() {
@@ -23,6 +27,18 @@ describe('Resources', function() {
 	});
 
 	describe('data', function() {
+		it('should return session storage contents', function(done) {
+			branch.data(function(err, data) {
+				assert.equal(data.identity, utils.whiteListSessionData(utils.readStore(branch._storage)).identity_id, 'identity matches');
+				assert.equal(data.has_app, utils.whiteListSessionData(utils.readStore(branch._storage)).identity_id, 'has_app matches');
+				assert.equal(data.referring_identity, utils.whiteListSessionData(utils.readStore(branch._storage)).identity_id, 'referring_identity matches');
+				assert.equal(data.data, utils.whiteListSessionData(utils.readStore(branch._storage)).identity_id, 'data matches');
+				done();
+			});
+		});
+	});
+
+	describe('track', function() {
 		var expectedRequest = {
 			"event": "test_event",
 			"metadata": utils.merge({
@@ -34,80 +50,10 @@ describe('Resources', function() {
 			"session_id": branch.session_id
 		};
 
-		it('should pass in app_id and browser_fingerprint_id', function(done) {
-			branch.track(expectedRequest["event"], done);
-		});
-
-		/*
-		it('should pass in app_id and browser_fingerprint_id', function(done) {
-			server.request(resources.open, params({ "is_referrable": 1 }), storage(), done);
-
+		it('should return session storage contents', function(done) {
+			branch.track(expectedRequest["event"], expectedRequest["metadata"], done);
 			assert.equal(requests.length, 1, 'Request made');
-			assert.equal(requests[0].url, 'https://api.branch.io/v1/open', 'Endpoint correct');
-			assert.equal(requests[0].method, 'POST', 'Method correct');
-			assert.equal(requests[0].requestBody, "app_id=" + app_id + "&identity_id=" + identity_id + "&is_referrable=1&browser_fingerprint_id=" + browser_fingerprint_id, 'Data correct');
-
-			requests[0].respond(200, { "Content-Type": "application/json" }, '{ "session_id": 123 }');
 		});
-
-		it('should pass as a jsonp request', function(done) {
-			storage()['setItem']('use_jsonp', true);
-			server.request(resources.open, params({ "is_referrable": 1 }), storage(), done);
-			assert.equal(requests.length, 1, 'Request made');
-			assert.equal(requests[0].url, 'https://api.branch.io/v1/open', 'Endpoint correct');
-			assert.equal(requests[0].method, 'JSONP', 'Method correct');
-			done();
-		});
-
-		it('should fail without is_referrable', function(done) {
-			server.request(resources.open, params(), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open missing parameter is_referrable");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-
-		it('should fail without app_id', function(done) {
-			server.request(resources.open, params({ }, [ 'app_id' ]), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open missing parameter app_id");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-
-		it('should fail without browser_fingerprint_id', function(done) {
-			server.request(resources.open, params({ is_referrable: 1 }, [ 'browser_fingerprint_id' ]), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open missing parameter browser_fingerprint_id");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-
-		// param format and type tests
-		it('should fail with incorrect app_id format', function(done) {
-			server.request(resources.open, params({ "app_id": "ahd7393j" }), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open, parameter app_id is not in the proper format");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-
-		it('should fail with link_identifier as number, not string', function(done) {
-			server.request(resources.open, params({ "link_identifier": 45433, "is_referrable": 1 }), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open, parameter link_identifier is not a string");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-
-		it('should fail with is_referrable as string, not number', function(done) {
-			server.request(resources.open, params({ "is_referrable": "1" }), storage(), function(err) {
-				assert.equal(err.message, "API request /v1/open, parameter is_referrable is not a number");
-				done();
-			});
-			assert.equal(requests.length, 0, 'No request made');
-		});
-*/
 	});
 });
 
