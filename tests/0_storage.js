38c080404c19113fefa2d6a7af22bfc44ccb4d28
Dmitri Gaskin <dmitrig01@gmail.com>
2015-03-12 14:38:25 -0700

    remove unnecessary dones

--- tests/0_storage.js
+++ tests/0_storage.js
@@ -6,36 +6,31 @@ describe('storage', function() {
 	var itemValue = 'value';
 	var assert = testUtils.unplanned();
 
-	it('should set an item', function(done) {
+	it('should set an item', function() {
 		storage.setItem(itemKey, itemValue);
 		assert.equal(storage._store[itemKey], itemValue, 'key / vaue stored');
-		done();
 	})
 
-	it('shold get stored item with key', function(done) {
+	it('shold get stored item with key', function() {
 		var item = storage.getItem(itemKey);
 		assert.equal(item, itemValue, 'correct value with key');
-		done();
 	});
 
-	it('should return null for an unstored item', function(done) {
+	it('should return null for an unstored item', function() {
 		var item = storage.getItem('not_an_item');
 		assert.equal(item, null, 'returned null');
-		done();
 	});
 
-	it('should remove an item', function(done) {
+	it('should remove an item', function() {
 		storage.removeItem(itemKey);
 		var item = storage.getItem(itemKey);
 		assert.equal(item, null, 'returned null');
-		done();
 	});
 
-	it('should clear all items', function(done) {
+	it('should clear all items', function() {
 		storage.setItem('key1', 'value1');
 		storage.setItem('key2', 'value2');
 		storage.clear();
 		assert.deepEqual(storage._store, { }, 'Storage cleared');
-		done();
 	});
 });
--- tests/0_utils.js
+++ tests/0_utils.js
@@ -4,17 +4,16 @@ describe('utils', function() {
 	var assert = testUtils.unplanned();
 
 	describe('base64encode', function() {
-		it('should encode a string', function(done) {
+		it('should encode a string', function() {
 			var string = 'test string to encode';
 			var encoded = 'dGVzdCBzdHJpbmcgdG8gZW5jb2Rl';
 			var testEncoded = utils.base64encode(string);
 			assert.equal(encoded, testEncoded, 'Correctly encoded');
-			done();
 		});
 	});
 
 	describe('merge', function() {
-		it('should merge to objects', function(done) {
+		it('should merge to objects', function() {
 			var obj1 = { "simple": "object" };
 			var obj2 = {
 				"nested": {
@@ -29,12 +28,11 @@ describe('utils', function() {
 			};
 			var mergedObject = utils.merge(obj1, obj2);
 			assert.deepEqual(mergedObject, expectedMerged, 'Correctly merged');
-			done();
 		});
 	});
 
 	describe('whiteListSessionData', function() {
-		it('should filter unwanted params', function(done) {
+		it('should filter unwanted params', function() {
 			var data = {
 				"data": {
 					"sample": "data"
@@ -47,10 +45,9 @@ describe('utils', function() {
 			var whiteListedData = utils.whiteListSessionData(data);
 			delete data["unwanted"];
 			assert.deepEqual(whiteListedData, data, 'Filtered unwanted param');
-			done();
 		});
 
-		it('should make missing params null', function(done) {
+		it('should make missing params null', function() {
 			var data = {
 				"data": {
 					"sample": "data"
@@ -60,89 +57,76 @@ describe('utils', function() {
 			};
 			var whiteListedData = utils.whiteListSessionData(data);
 			assert.equal(null, data["has_app"], 'Make has_app null');
-			done();
 		});
 	});
 
 	describe('message', function() {
-		it('should produce a missing param message', function(done) {
+		it('should produce a missing param message', function() {
 			var message = utils.message(utils.messages.missingParam, [ 'endpoint', 'param' ]);
 			assert.equal('API request endpoint missing parameter param', message, 'Expected missing param message produced');
-			done();
 		});
 
-		it('should produce an invalid param type message', function(done) {
+		it('should produce an invalid param type message', function() {
 			var message = utils.message(utils.messages.invalidType, [ 'endpoint', 'param', 'type' ]);
 			assert.equal('API request endpoint, parameter param is not type', message, 'Expected invalid param type message produced');
-			done();
 		});
 
-		it('should produce a Branch SDK non init message', function(done) {
+		it('should produce a Branch SDK non init message', function() {
 			var message = utils.message(utils.messages.nonInit);
 			assert.equal('Branch SDK not initialized', message, 'Branch SDK not initialized');
-			done();
 		});
 
-		it('should produce a Branch SDK already init message', function(done) {
+		it('should produce a Branch SDK already init message', function() {
 			var message = utils.message(utils.messages.existingInit);
 			assert.equal('Branch SDK already initilized', message, 'Branch SDK already initialized');
-			done();
 		});
 
-		it('should produce a missing app id', function(done) {
+		it('should produce a missing app id', function() {
 			var message = utils.message(utils.messages.missingAppId);
 			assert.equal('Missing Branch app ID', message, 'Branch app id missing');
-			done();
 		});
 
-		it('should produce a call branch init first', function(done) {
+		it('should produce a call branch init first', function() {
 			var message = utils.message(utils.messages.callBranchInitFirst);
 			assert.equal('Branch.init must be called first', message, 'Branch must be called first message');
-			done();
 		});
 
-		it('should produce a timeout message', function(done) {
+		it('should produce a timeout message', function() {
 			var message = utils.message(utils.messages.timeout);
 			assert.equal('Request timed out', message, 'Request timed out');
-			done();
 		});
 
-		it('should produce a missing URL error', function(done) {
+		it('should produce a missing URL error', function() {
 			var message = utils.message(utils.messages.missingUrl);
 			assert.equal('Required argument: URL, is missing', message, 'Missing url');
-			done();
 		});
 	});
 
 	describe("getParamValue", function() {
-		it('should return search param value', function(done) {
+		it('should return search param value', function() {
 			testUtils.go('?test=testsearch');
 			var value = utils.getParamValue('test');
 			assert.equal('testsearch', value, 'Returns search param');
-			done();
 		});
 
-		it('should return undefined if not set', function(done) {
+		it('should return undefined if not set', function() {
 			testUtils.go('');
 			var value = utils.getParamValue('test');
 			assert.equal(undefined, value, 'returns undefined');
-			done();
 		});
 	});
 
 	describe("hashValue", function() {
-		it('should return hash param value', function(done) {
+		it('should return hash param value', function() {
 			testUtils.go('#test:testhash');
 			var value = utils.hashValue('test');
 			assert.equal('testhash', value, 'Returns hash param');
-			done();
 		});
 
-		it('should return undefined if not set', function(done) {
+		it('should return undefined if not set', function() {
 			testUtils.go('');
 			var value = utils.hashValue('test');
 			assert.equal(undefined, value, 'returns undefined');
-			done();
 		});
 	});
 });
