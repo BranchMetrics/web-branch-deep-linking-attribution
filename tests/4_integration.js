b4d150bdd94050c3f9a77024b5026d704e68bcf5
Dmitri Gaskin <dmitrig01@gmail.com>
2015-03-12 14:12:51 -0700

    Add stub for integration tests

---
+++ tests/4_integration.js
@@ -0,0 +1,9 @@
+
+describe('Integration tests', function() {
+	// TODO: would be great to write some tests here which call the Branch API
+	// functions but stub out XHR.
+
+	// With the above, we can actually run these tests against the *minified*
+	// version of the SDK, so we can make sure that we didn't do anything to
+	// screw up the Closure Compiler.
+});
