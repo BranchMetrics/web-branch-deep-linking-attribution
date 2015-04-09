e7440a71adeb09dfb13e8b0139c0aaeded41a958
Dmitri Gaskin <dmitrig01@gmail.com>
2015-04-08 20:03:47 -0700

    Fix filename printing

--- lint.sh
+++ lint.sh
@@ -3,20 +3,19 @@
 BAD=0
 JSHINT
 
+exec 5>&1
+
 for file in $( find src tests | grep '\d_' ); do
   jshintrc=".jshintrc"
   if [[ $file == tests* ]]; then
     jshintrc=".jshintrc-test"
   fi
 
-  cat $file | perl -pe 's#goog.provide\('"'(.*)'"'\);?#/* exported $1 */ var $1;#' | perl -pe 's#goog.require\('"'(.*)'"'\);?#/* global $1:false */#' | jshint -c $jshintrc -
-
-  if [ "$?" != 0 ]; then
-    echo "jshint error in $file"
-    echo ""
-    echo ""
+  ERRORS=$( cat $file | perl -pe 's#goog.provide\('"'(.*)'"'\);?#/* exported $1 */ var $1;#' | perl -pe 's#goog.require\('"'(.*)'"'\);?#/* global $1:false */#' | jshint -c $jshintrc - | perl -pe "s#stdin#$file#" | tee >(cat - >&5) )
+  if [ "$ERRORS" != "" ]; then
     BAD=1
   fi;
+
   ERRORS=$(jscs -c .jscsrc $file | grep -e '\d code style errors found' | wc -l | tr -d ' \t')
   if [ "$ERRORS" != 0 ]; then
     jscs -c .jscsrc $file
