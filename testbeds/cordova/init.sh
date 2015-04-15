1b01f5db1ab652fd2a9db93c37e17d1aec8d869d
Dmitri Gaskin <dmitrig01@gmail.com>
2015-04-14 21:26:36 -0700

    try this?

--- testbeds/cordova/init.sh
+++ testbeds/cordova/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cordova platform add ios android
 cordova plugin add org.apache.cordova.console de.appplant.cordova.plugin.email-composer
 ln -s `cd ../.. && pwd` plugins/io.branch.sdk
+cordova platform add ios android
