be3c18c80143def0f13b0c2a5cb7e39339f4b712
Dmitri Gaskin <dmitrig01@gmail.com>
2015-04-15 14:38:38 -0700

    add web-config.js

---
+++ test/web-config.js
@@ -0,0 +1,14 @@
+/**
+ * Just a couple of variables that shouldn't change very often...
+ */
+goog.provide('config');
+
+config.link_service_endpoint = 'https://bnc.lt';
+config.api_endpoint = 'https://api.branch.io';
+config.version = '1.3.4';
+
+/** @define {boolean} */
+config.WEB_BUILD = true;
+
+/** @define {boolean} */
+config.CORDOVA_BUILD = false;
