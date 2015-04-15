7a6493aaa7e564974db9ce802fca2626db5ea562
Dmitri Gaskin <dmitrig01@gmail.com>
2015-04-15 15:33:22 -0700

    More unification between the two SDKs

--- test/branch-deps.js
+++ test/branch-deps.js
@@ -11,7 +11,7 @@ goog.addDependency("../../../../../src/1_banner_html.js", ['banner_html'], ['ban
 goog.addDependency("../../../../../src/1_resources.js", ['resources'], ['utils', 'config']);
 goog.addDependency("../../../../../src/2_banner.js", ['banner'], ['banner_utils', 'banner_css', 'banner_html', 'config', 'utils']);
 goog.addDependency("../../../../../src/3_branch.js", ['Branch'], ['utils', 'resources', 'Server', 'banner', 'Queue', 'storage', 'config', 'goog.json']);
-goog.addDependency("../../../../../src/4_initialization.js", ['branch_instance'], ['Branch', 'utils', 'config']);
+goog.addDependency("../../../../../src/4_initialization.js", ['branch_instance'], ['Branch', 'config']);
 goog.addDependency("../../../../../src/extern.js", [], []);
 goog.addDependency("../../../../../src/onpage.js", [], []);
 goog.addDependency("../../../../../test/0_queue.js", [], ['Queue']);
