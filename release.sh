375c2203b6bf0aa0a457ac640e3ceaf46396f117
Scott Hasbrouck <scott@ginkgotree.com>
2015-05-08 19:56:50 -0400

    clarification and remove test file

--- release.sh
+++ release.sh
@@ -88,7 +88,7 @@ then
 fi
 
 echo "Integration Guide URL: https://github.com/BranchMetrics/Branch-Integration-Guides/blob/master/smart-banner-guide.md"
-read -p "Did you update the Branch Integration Guide?" -n 1 -r
+read -p "Did you update the Branch Integration Guide, specifically the Javascript Snippet and App Banner?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
--- test.sh
+++
@@ -1,7 +0,0 @@
-echo "Integration Guide URL: https://github.com/BranchMetrics/Branch-Integration-Guides/blob/master/smart-banner-guide.md"
-read -p "Did you update the Branch Integration Guide?" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	echo "Ok"
-fi
