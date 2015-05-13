d43f79805ef60ff04db521e2e140ebf01cb2fd6c
Scott Hasbrouck <scott@ginkgotree.com>
2015-05-13 16:46:51 -0400

    fix s3 upload release script

--- release.sh
+++ release.sh
@@ -67,7 +67,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	make dist/web/build.min.js.gz
-	aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/web/build.min.js.gz s3://branch-cdn/branch-v$VERSION.min.js  --acl public-read
+	aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/web/build.min.js.gz s3://branch-cdn/branch-$VERSION.min.js  --acl public-read
 	aws s3 cp testbeds/web/example.html s3://branch-cdn/example.html --acl public-read
 fi
 
