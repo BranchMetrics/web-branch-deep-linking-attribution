760fac4ee59815103ae79ed18523b87d0655b505
Scott Hasbrouck <scott@ginkgotree.com>
2015-05-19 12:30:34 -0700

    ugh, remove retries to 4xx

--- test/4_integration.js
+++ test/4_integration.js
@@ -84,10 +84,6 @@ describe('Integration tests', function() {
 			});
 			requests[0].callback(browser_fingerprint_id);
 			requests[1].respond(400);
-			clock.tick(250);
-			requests[2].respond(400);
-			clock.tick(250);
-			requests[3].respond(400);
 		});
 
 		it('should attempt 5xx error three times total', function(done) {
