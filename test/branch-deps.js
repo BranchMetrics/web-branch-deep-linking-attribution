897f5a219fac73f0935edde3583d232c12aa6750
Scott Hasbrouck <scott@ginkgotree.com>
2015-06-02 18:09:20 -0700

    renames web session file

--- test/branch-deps.js
+++ test/branch-deps.js
@@ -3,9 +3,9 @@
 goog.addDependency("../../../../../src/0_banner_utils.js", ['banner_utils'], ['web_session', 'storage']);
 goog.addDependency("../../../../../test/web-config.js", ['config'], []);
 goog.addDependency("../../../../../src/0_queue.js", ['Queue'], []);
-goog.addDependency("../../../../../src/0_session.js", ['web_session'], ['goog.json']);
 goog.addDependency("../../../../../src/0_storage.js", ['storage'], []);
 goog.addDependency("../../../../../src/0_utils.js", ['utils'], ['goog.json', 'storage', 'web_session', 'config']);
+goog.addDependency("../../../../../src/0_web_session.js", ['web_session'], ['goog.json']);
 goog.addDependency("../../../../../src/1_api.js", ['Server'], ['utils', 'goog.json', 'storage']);
 goog.addDependency("../../../../../src/1_banner_css.js", ['banner_css'], ['banner_utils']);
 goog.addDependency("../../../../../src/1_banner_html.js", ['banner_html'], ['banner_utils', 'utils', 'storage']);
