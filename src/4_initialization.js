1261dfc9e48e0365de1a635e2a16a90579a1eac6
Scott Hasbrouck <scott@ginkgotree.com>
2015-06-08 16:45:59 -0700

    puts branch string in array for AMD context

--- src/4_initialization.js
+++ src/4_initialization.js
@@ -23,12 +23,13 @@ if (window['branch'] && window['branch']['_q']) {
 
 // AMD
 if (typeof define === 'function' && define.amd) {
-	define('branch', function() { return branch_instance; });
+	define(['branch'], function() { return branch_instance; });
 }
 // CommonJS-like environments that support module.exports
 else if (typeof exports === 'object') {
 	module.exports = branch_instance;
 }
+
 // Always make a global.
 if (window) { window['branch'] = branch_instance; }
 
