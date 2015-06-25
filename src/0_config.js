be50fd576c580ebe5fc2277cb2f41b7f74cbc965
Scott Hasbrouck <hello@scotthasbrouck.com>
2015-06-25 14:57:40 -0700

    Tagging release v1.5.8

--- src/0_config.js
+++ src/0_config.js
@@ -5,7 +5,7 @@ goog.provide('config');
 
 config.link_service_endpoint = 'https://bnc.lt';
 config.api_endpoint = 'https://api.branch.io';
-config.version = '1.5.7';
+config.version = '1.5.8';
 
 /** @define {boolean} */
 var WEB_BUILD = false; // jshint unused:false
