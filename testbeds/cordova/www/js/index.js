91e70bc9d3ff179ef1c3fa53e2af382543d5da7f
Scott Hasbrouck <hello@scotthasbrouck.com>
2015-07-04 19:40:42 -0700

    cleans up cordova testbed app and fixes close() bug

--- testbeds/cordova/www/js/index.js
+++ testbeds/cordova/www/js/index.js
@@ -1,379 +1,177 @@
-
-// Intercept form submission.
-function dummy() {
-}
+function interceptFormSubmission() { }
 
 var app = {
-    // Application Constructor
+    // ------------------ Inititalization & Utility Methods ------------------
+
     initialize: function() {
-        this.bindEvents();
+        app.branch_key = "key_live_hohBTjb6eJS9jou8Tl1kIhddokjfbXJF";
+        document.addEventListener('deviceready', app.onDeviceReady, false);
     },
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('resume', this.onResume, false);
+    setElement: function(element, value) { if (element && element.length) { document.getElementById(element).innerHTML = value; } },
+
+    setStatus: function(status) { if (status && status.length > 0) { app.setElement("status", status); } },
+
+    getElementValue: function(element) {
+        var elementRef = document.getElementById(element);
+        return elementRef ? document.getElementById(element).value : "";
     },
 
-    // deviceready Event Handler
-    onDeviceReady: function() {
-        console.log("Device ready");
-        branch.setDebug(true);
-    	console.log("Sending init");
-        branch.init('key_live_hohBTjb6eJS9jou8Tl1kIhddokjfbXJF', { isReferrable: true }, function(err, data) {
-        	app.initComplete(err, data);
+    executeBranchMethod: function(method, arguments, element, callback) {
+        app.setStatus("branch." + method + "() Called");
+        arguments.push(function(err, data) {
+            if (err) {
+                app.setStatus(method + "() " + err);
+                app.setElement(element, err);
+            }
+            else {
+                app.setStatus(method + "() Successful");
+                app.setElement(element, typeof data == "object" ? JSON.stringify(data) : data);
+            }
+            if (callback && typeof callback == "function" ) { callback(err, data); }
         });
-        var statusEle = document.getElementById("status");
-        statusEle.innerHTML = "Ready..."
+        branch[method].apply(branch, arguments);
     },
 
-    // pause Event Handler
+    // ------------------ Lifecycle Methods ------------------
+
+    onDeviceReady: function() {
+        branch.setDebug(true);
+        document.addEventListener('resume', app.onResume, false);
+        document.addEventListener('pause', app.onPause, false);
+        app.setStatus("onDeviceReady()");
+        branch.init(app.branch_key, { isReferrable: true }, function(err, data) {
+            app.initComplete(err, data);
+        });
+    },
+
     onPause: function() {
-    	console.log("Sending close.");
-    	branch.close(function(err) {
-            var statusEle = document.getElementById("status");
-        	if (err) {
-                console.log("Close error: " + err);
-        		statusEle.innerHTML = err;
-        	} else {
-                console.log("Close complete.");
-                statusEle.innerHTML = "Ok";
-        	}
-   		});
+        app.setStatus("onPause(): Sending Close");
+        branch.close(function(err) {
+            if (err) { app.setStatus("Close error: ", err); }
+            else { app.setStatus("Close finished successfully"); }
+        });
     },
 
-    // resume Event Handler
     onResume: function() {
-    	console.log("Sending init");
-        branch.init('key_live_hohBTjb6eJS9jou8Tl1kIhddokjfbXJF', { isReferrable: true }, function(err, data) {
-        	app.initComplete(err, data);
+        app.setStatus("onResume()");
+        branch.init(app.branch_key, { isReferrable: true }, function(err, data) {
+            app.initComplete(err, data);
         });
     },
 
     initComplete: function(err, data) {
-        var statusEle = document.getElementById("status");
-        var firstEle = document.getElementById("first");
-        var latestEle = document.getElementById("latest");
-    	if (err) {
-            console.log("Init error: " + err);
-    		statusEle.innerHTML = err;
-    	} else {
-            console.log("Init complete: " + JSON.stringify(data));
-            statusEle.innerHTML = "Ok";
+        if (err) { app.setStatus("Init error: ", err); }
+        else {
+            app.setStatus("Branch SDK Init Success");
             branch.first(function(err, data) {
-            	if (data.data && !(data.data === "null")) {
-            		first.innerHTML = JSON.stringify(data.data);
-            	} else {
-            		first.HTML = "{}";
-            	}
+                if (data.data && !(data.data === "null")) { app.setElement("first", JSON.stringify(data.data)); }
+                else { app.setElement("first", "{ }"); }
             });
             branch.data(function(err, data) {
-            	if (data.data && !(data.data === "null")) {
-            		latest.innerHTML = JSON.stringify(data.data);
-            	} else {
-            		latest.HTML = "{}";
-            	}
+                if (data.data && !(data.data === "null")) { app.setElement("latest", JSON.stringify(data.data)); }
+                else { app.setElement("latest", "{ }"); }
             });
-    	}
+        }
     },
+
+    //------------------ Branch SDK Methods ------------------
 
     createUrl: function() {
-        console.log("Create URL clicked");
-        var aliasEle = document.getElementById("alias");
-        var channelEle = document.getElementById("channel");
-        var stageEle = document.getElementById("stage");
-        var typeEle = document.getElementById("url_type");
-        var featureEle = document.getElementById("feature");
-        var tagsEle = document.getElementById("tags");
-        var paramsEle = document.getElementById("params");
-        var tags = null;
-        if (tagsEle.value) {
-            tags = tagsEle.value.split(',');
-        }
-        var params = {};
-        var current = new Date();
-        params.creation_data = current.toString();
-        if (paramsEle.value) {
-            var prms = paramsEle.value.split(',');
-            if (prms.length > 0) {
-                params.params = prms;
-            }
-        }
-        var vals = {};
-       	vals.type = parseInt(typeEle.value);
-        if (featureEle.value) {
-        	if (!(featureEle.value === "none")) {
-            	vals.feature = featureEle.value;
-        	}
-        }
-        if (aliasEle.value) {
-        	vals.alias = aliasEle.value;
-        }
-        if (channelEle.value) {
-        	vals.channel = channelEle.value;
-        }
-        if (stageEle.value) {
-        	vals.stage = stageEle.value;
-        }
-        if (tags) {
-        	vals.tags = tags;
-        }
-        vals.data = params;
-        console.log("Sending: " + JSON.stringify(vals));
-        branch.link(vals, function(err, url) {
-            var statusEle = document.getElementById("status");
-            var urlEle = document.getElementById("url_label");
-            if (!err) {
-                statusEle.innerHTML = "Ok";
-                url_label.innerHTML = url;
-            } else {
-                statusEle.innerHTML = err;
-                url_label.innerHTML = err;
-            }
-        });
+        var params = { };
+        params.data = { };
+        params.data.creation_date = new Date().toString();
+        if (app.getElementValue("feature") && app.getElementValue("feature") != "none") { params.feature = app.getElementValue("feature"); }
+        if (app.getElementValue("alias")) { params.alias = app.getElementValue("alias"); }
+        if (app.getElementValue("channel")) { params.channel = app.getElementValue("channel"); }
+        if (app.getElementValue("stage")) { params.stage = app.getElementValue("stage"); }
+        if (app.getElementValue("tags")) { params.tags = app.getElementValue("tags").split(','); }
+
+        app.executeBranchMethod("link", [ params ], "url_label");
     },
 
-    login: function() {
-    	console.log("Sending login");
-    	var nameEle = document.getElementById("name");
-    	var name = nameEle.value;
-    	if (name && name.length > 0) {
-    		branch.setIdentity(name, function (err, data) {
-    			var statusEle = document.getElementById("status");
-				var usernameEle = document.getElementById("username");
-    			if (err) {
-    				console.log("Login error: " + err);
-    				statusEle.innerHTML = err;
-    				usernameEle.innerHTML = "";
-    			} else {
-    				console.log("Login successful");
-    				statusEle.innerHTML = "Ok";
-    				usernameEle.innerHTML = name;
-    			}
-    		});
-    	}
+    setIdentity: function() {
+        var name = app.getElementValue("name");
+        if (name && name.length > 0) {
+            app.executeBranchMethod("setIdentity", [ name ], "name", function(err, data) {
+                if (!err) { app.setElement("username", name); }
+            });
+        }
+        else {
+            app.setStatus("setIdentity() no login name specififed");
+            app.setElement("username", "no login name specififed");
+        }
     },
 
     logout: function() {
-    	console.log("Sending logout");
-		branch.logout(function (err) {
-			var statusEle = document.getElementById("status");
-			var usernameEle = document.getElementById("username");
-			usernameEle.innerHTML = "";
-			if (err) {
-				console.log("Logout error: " + err);
-				statusEle.innerHTML = err;
-			} else {
-				console.log("Logout successful");
-				statusEle.innerHTML = "Ok";
-			}
-		});
-    },
-
-    sendEmail: function() {
-        var urlEle = document.getElementById("url_label");
-        var message = "Follow this link to the new app! " + urlEle.innerHTML;
-        cordova.plugins.email.open(
-            {
-                subject: "Check out this app!",
-                body: message
-            });
-    },
-
-    completeAction: function() {
-    	var eventEle = document.getElementById("event");
-    	var statusEle = document.getElementById("status");
-    	if (eventEle.value) {
-            var params = {};
-            var current = new Date();
-            params.creation_data = current.toString();
-            console.log("Sending track.");
-            branch.track(eventEle.value, params, function(err, data) {
-            	if (err) {
-            		console.log("Track error: " + err);
-            		statusEle.innerHTML = err;
-            	} else {
-            		console.log("Track complete.");
-            		statusEle.innerHTML = "Ok";
-            	}
-            });
-    	}
-    },
-
-    loadActions: function() {
-    	var actionsEle = document.getElementById("actions");
-    	var statusEle = document.getElementById("status");
-        var params = {};
-        var current = new Date();
-        params.creation_data = current.toString();
-        console.log("Sending referrals");
-        branch.referrals(function(err, data) {
-        	if (err) {
-        		console.log("Referrals error: " + err);
-        		statusEle.innerHTML = err;
-        		actionsEle.innerHTML = err;
-        	} else {
-        		console.log("Referrals complete: " + JSON.stringify(data));
-        		statusEle.innerHTML = "Ok";
-        		actionsEle.innerHTML = JSON.stringify(data);
-        	}
+        app.executeBranchMethod("logout", [ ], "username", function(err, data) {
+            if (!err) { app.setElement("username", name); }
         });
     },
 
+    sendEmail: function() {
+        cordova.plugins.email.open({
+            subject: "Check out this app!",
+            body: "Follow this link to the new app! " + app.getElementValue("url_label")
+        });
+    },
+
+    completeAction: function() {
+        var eventName = app.getElementValue("event");
+        if (eventName && eventName.length > 0) {
+            var params = { "creation_date": Date().toString() };
+            app.executeBranchMethod("track", [ eventName, params], "");
+        }
+    },
+
+    referrals: function() {
+        app.executeBranchMethod("referrals", [ ], "actions");
+    },
+
     createCode: function() {
-    	var prefixEle = document.getElementById("prefix");
-    	var amountEle = document.getElementById("amount");
-    	var bucketEle = document.getElementById("bucket");
-    	var locationEle = document.getElementById("location");
-    	var typeEle = document.getElementById("calculation_type");
-    	var expirationEle = document.getElementById("exp");
-    	var codeEle = document.getElementById("code_label");
-    	var statusEle = document.getElementById("status");
-
-    	var val = {};
-    	if (prefixEle.value) {
-    		val.prefix = prefixEle.value;
-    	}
-    	if (bucketEle.value) {
-    		val.bucket = bucketEle.value;
-    	} else {
-    		val.bucket = "default";
-    	}
-    	if (amountEle.value) {
-    		val.amount = parseInt(amountEle.value);
-    	} else {
-    		val.amount = 10;
-    	}
-    	if (expirationEle.value) {
-    		val.expiration = expirationEle.value;
-    	}
-    	val.calculation_type = parseInt(typeEle.value);
-    	val.location = parseInt(locationEle.value);
-
-    	console.log("Sending get code: " + JSON.stringify(val));
-    	branch.getCode(val, function(err, data) {
-    		if (err) {
-    			console.log("Error getting code: " + err);
-    			statusEle.innerHTML = err;
-    			codeEle.innerHTML = err;
-    		} else {
-    			console.log("Get code successful: " + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			codeEle.innerHTML = data.referral_code;
-    		}
-    	});
+        var params = { };
+        params.amount = app.getElementValue("amount") ? parseInt(app.getElementValue("amount")) : 1;
+        params.calculation_type = app.getElementValue("calculation_type") ? parseInt(app.getElementValue("calculation_type")) : 1;
+        params.location = app.getElementValue("location") ? parseInt(app.getElementValue("location")) : 2;
+        if (app.getElementValue("bucket")) { params.bucket = app.getElementValue("bucket"); }
+        if (app.getElementValue("prefix")) { params.prefix = app.getElementValue("prefix"); }
+        app.executeBranchMethod("getCode", [ params ], "code_label");
     },
 
     validateCode: function() {
-    	var codeEle = document.getElementById("av_code");
-    	var codeStatusEle = document.getElementById("code_status_label");
-    	var statusEle = document.getElementById("status");
-    	var code = codeEle.value;
-    	if (!code) {
-    		return;
-    	}
-    	console.log("Sending validate code for: " + code);
-    	branch.validateCode(code, function(err, data) {
-    		if (err) {
-    			console.log("Error validating code: " + err);
-    			statusEle.innerHTML = err;
-    			codeStatusEle.innerHTML = err;
-    		} else {
-    			console.log("Validation successful." + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			codeStatusEle.innerHTML = code + " is valid.";
-    		}
-    	});
+        var code = app.getElementValue("av_code");
+        if (!code) {
+            app.setElement("code_status_label", "Error: no code specified");
+            return app.setStatus("applyCode() Error: no code specified");
+        }
+        app.executeBranchMethod("validateCode", [ code ], "code_status_label", function(err,data) {
+            if (!err) { app.setElement("code_status_label", "Code Valid"); }
+        });
     },
 
     applyCode: function() {
-    	var codeEle = document.getElementById("av_code");
-    	var codeStatusEle = document.getElementById("code_status_label");
-    	var statusEle = document.getElementById("status");
-    	var code = codeEle.value;
-    	if (!code) {
-    		return;
-    	}
-    	console.log("Sending apply code for: " + code);
-    	branch.applyCode(code, function(err, data) {
-    		if (err) {
-    			console.log("Error applying code: " + err);
-    			statusEle.innerHTML = err;
-    			codeStatusEle.innerHTML = err;
-    		} else {
-    			console.log("Apply successful." + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			codeStatusEle.innerHTML = code + " applied.";
-    		}
-    	});
+        var code = app.getElementValue("av_code");
+        if (!code) {
+            app.setElement("code_status_label", "Error: no code specified");
+            return app.setStatus("applyCode() Error: no code specified");
+        }
+        app.executeBranchMethod("applyCode", [ code ], "code_status_label", function(err,data) {
+            if (!err) { app.setElement("code_status_label", "Code Applied"); }
+        });
     },
 
-
-    redeemRewards: function() {
-    	var amountEle = document.getElementById("rewards_amount");
-    	var bucketEle = document.getElementById("rewards_bucket");
-    	var rewardsStatusEle = document.getElementById("rewards_status_label")
-    	var statusEle = document.getElementById("status");
-    	console.log("Sending redeem rewards");
-    	branch.redeem(parseInt(amountEle.value), bucketEle.value, function(err, data) {
-    		if (err) {
-    			console.log("Error redeeming rewards: " + err);
-    			statusEle.innerHTML = err;
-    			rewardsStatusEle.innerHTML = err;
-    		} else {
-    			console.log("Redeem successful." + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			rewardsStatusEle.innerHTML = "Credits redeemed.";
-    		}
-    	});
+    redeem: function() {
+        app.executeBranchMethod("redeem",
+            [ parseInt(app.getElementValue("rewards_amount")), app.getElementValue("rewards_bucket") ],
+            "rewards_status_label");
     },
 
-
-    getCredits: function() {
-    	var amountEle = document.getElementById("rewards_amount");
-    	var bucketEle = document.getElementById("rewards_bucket");
-    	var rewardsStatusEle = document.getElementById("rewards_status_label")
-    	var statusEle = document.getElementById("status");
-    	console.log("Sending get credits");
-    	branch.credits(function(err, data) {
-    		if (err) {
-    			console.log("Error getting credits: " + err);
-    			statusEle.innerHTML = err;
-    			rewardsStatusEle.innerHTML = err;
-    		} else {
-    			console.log("Get credits successful." + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			rewardsStatusEle.innerHTML = JSON.stringify(data);
-    		}
-    	});
+    credits: function() {
+        app.executeBranchMethod("credits", [ ], "rewards_status_label");
     },
 
-
-    getCreditHistory: function() {
-    	var amountEle = document.getElementById("rewards_amount");
-    	var bucketEle = document.getElementById("rewards_bucket");
-    	var rewardsStatusEle = document.getElementById("rewards_status_label")
-    	var statusEle = document.getElementById("status");
-    	var values = {};
-    	if (bucketEle.value) {
-    		values.bucket = bucketEle.value;
-    	}
-    	console.log("Sending redeem rewards: " + JSON.stringify(values));
-    	branch.creditHistory(values, function(err, data) {
-    		if (err) {
-    			console.log("Error getting history: " + err);
-    			statusEle.innerHTML = err;
-    			rewardsStatusEle.innerHTML = err;
-    		} else {
-    			console.log("Get history successful." + JSON.stringify(data));
-    			statusEle.innerHTML = "Ok";
-    			rewardsStatusEle.innerHTML = JSON.stringify(data);
-    		}
-    	});
+    creditHistory: function() {
+        var params = { "bucket": app.getElementValue("rewards_bucket") || "" };
+        app.executeBranchMethod("creditHistory", [ params ], "rewards_bucket");
     }
 };
-
-app.initialize();
