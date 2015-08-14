0e8824eba42e89c2ad9266b229cdd02a457fa90e
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-08-14 06:31:51 -0700

    revert non-essential changes in kirk-fixdata

--- src/0_utils.js
+++ src/0_utils.js
@@ -78,12 +78,12 @@ utils.message = function(message, params) {
  */
 utils.whiteListSessionData = function(data) {
 	return {
-		'data': ('data' in data) ? data.data : null,
-		'data_parsed': ('data_parsed' in data) ? data.data_parsed : null,
-		'has_app': ('has_app' in data) ? data.has_app : null,
-		'identity': ('identity' in data) ? data.identity : null,
-		'referring_identity': ('referring_identity' in data) ? data.referring_identity : null,
-		'referring_link': ('referring_link' in data) ? data.referring_link : null
+		'data': data.data || null,
+		'data_parsed': data.data_parsed || null,
+		'has_app': data.has_app || null,
+		'identity': data.identity || null,
+		'referring_identity': data.referring_identity || null,
+		'referring_link': data.referring_link || null
 	};
 };
 
