ac08375a488c42e0c8f46c6b8b5f81bd433fe48b
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-08-31 10:36:31 -0700

    make test/0_queue.js order independent

--- test/0_queue.js
+++ test/0_queue.js
@@ -3,13 +3,13 @@
 goog.require('task_queue');
 
 describe('task_queue', function() {
-	var queue = task_queue();
 	var clock = sinon.useFakeTimers();
-	var orderCalled = [];
-
 	var assert = testUtils.unplanned();
 
 	it('should queue a function and call it', function(done) {
+		var queue = task_queue();
+		var orderCalled = [];
+
 		queue(function(next) {
 			setTimeout(function() {
 				orderCalled.push(0);
@@ -23,24 +23,27 @@ describe('task_queue', function() {
 	});
 
 	it('should enqueue two functions, and call them in order', function(done) {
+		var queue = task_queue();
+		var orderCalled = [];
+
+		queue(function(next) {
+			setTimeout(function() {
+				orderCalled.push(0);
+				next();
+			}, 10);
+		});
+		assert.equal(undefined, orderCalled[0], 'Has not yet called function');
 		queue(function(next) {
 			setTimeout(function() {
 				orderCalled.push(1);
 				next();
 			}, 10);
 		});
-		assert.equal(undefined, orderCalled[1], 'Has not yet called function');
-		queue(function(next) {
-			setTimeout(function() {
-				orderCalled.push(2);
-				next();
-			}, 10);
-		});
 		clock.tick(10);
-		assert.equal(1, orderCalled[1], 'Called first function');
-		assert.equal(undefined, orderCalled[2], 'Has not yet called second function');
+		assert.equal(0, orderCalled[0], 'Called first function');
+		assert.equal(undefined, orderCalled[1], 'Has not yet called second function');
 		clock.tick(10);
-		assert.equal(2, orderCalled[2], 'Called second function');
+		assert.equal(1, orderCalled[1], 'Called second function');
 		done();
 	});
 });
