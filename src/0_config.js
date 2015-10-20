be7c548787568a66c9a19266c02ecfcf4de02c6f
Kirk Ke Tang <nus.thomaskirk@gmail.com>
2015-10-20 11:41:13 -0700

    minor

--- src/0_config.js
+++ src/0_config.js
@@ -5,6 +5,7 @@ goog.provide('config');
 
 config.link_service_endpoint = 'https://bnc.lt';
 config.api_endpoint = 'https://api.branch.io';
+config.api_endpoint = 'http://localhost:5001'; // @nocommit
 config.version = '1.7.0';
 
 /** @define {boolean} */
