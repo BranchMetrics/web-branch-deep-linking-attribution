cde8103e8180012243acaada95010eb6934e0c2c
John Saleigh <john@branch.io>
2015-10-29 13:51:08 -0700

    Allow "identity" to be returned from branch.init()

--- src/6_branch.js
+++ src/6_branch.js
@@ -332,6 +332,8 @@ Branch.prototype['init'] = wrap(
 			}
 			if (data['identity_id']) {
 				self.identity_id = data['identity_id'].toString();
+				// to make acceptable for utils.whiteListSessionData
+				data['identity'] = self.identity_id;
 			}
 			if (data['link']) {
 				self.sessionLink = data['link'];
