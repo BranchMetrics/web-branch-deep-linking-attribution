1f339d1439b9c57537494956844aa8cf5f102995
John Saleigh <john@branch.io>
2016-03-09 09:20:15 -0800

    Tagging release v2.1.0

--- test/web-config.js
+++ test/web-config.js
@@ -7,7 +7,7 @@ goog.provide('config');
 
 config.link_service_endpoint = 'https://bnc.lt';
 config.api_endpoint = 'https://api.branch.io';
-config.version = '2.0.3';
+config.version = '2.1.0';
 
 /** @define {boolean} */
 window.WEB_BUILD = true;
