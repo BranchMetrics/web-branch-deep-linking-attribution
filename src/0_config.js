9fca082642947b427ecbed52b8479d5300db49e6
John Saleigh <john@branch.io>
2016-04-27 10:26:58 -0700

    updated endpoints

--- src/0_config.js
+++ src/0_config.js
@@ -6,5 +6,6 @@ goog.provide('config');
 config.app_service_endpoint = 'https://app.link';
 config.link_service_endpoint = 'https://bnc.lt';
 // config.api_endpoint = 'https://api.branch.io';
-config.api_endpoint = 'http://54.193.112.65';
+// config.api_endpoint = 'http://54.193.112.65';
+config.api_endpoint = 'http://api.dev.branch.io';
 config.version = '2.2.0';
