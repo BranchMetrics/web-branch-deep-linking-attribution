3fa1fb9db358152f8bac54f5dd49d0cc994abca3
John Saleigh <john@branch.io>
2016-07-18 15:28:22 -0700

    WEBSDK-2 changes to allow for annotation to match the code

--- src/1_utils.js
+++ src/1_utils.js
@@ -28,9 +28,14 @@ utils.httpMethod = {
 	GET: 'GET'
 };
 
-/** @typedef {{destination: string, endpoint: string, method: utils._httpMethod,
- * params: ?Object.<string, _validator>, queryPart: ?Object.<string, _validator>,
- * jsonp: ?boolean }} */
+/** @typedef {{
+ * destination: string,
+ * endpoint: string,
+ * method: utils._httpMethod,
+ * params: ?Object.<string, _validator>,
+ * queryPart: ?Object.<string, _validator>,
+ * jsonp: ?boolean
+ * }} */
 utils.resource;
 
 /** @typedef {{listener: function(string):null, event: string}} */
