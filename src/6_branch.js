f2400eeb11027737035d82251fd1afab9ee9e299
Rubin Singh <rubinsingh@gmail.com>
2017-01-06 15:33:44 -0800

    fixing typo for autoappindexing function

--- src/6_branch.js
+++ src/6_branch.js
@@ -1773,14 +1773,14 @@ Branch.prototype['closeBanner'] = wrap(0, function(done) {
  * | --- | ---
  * | "androidPackageName" | Android App's package name
  * | "androidURL" | A custom scheme for your Android App such as: `example/home/cupertino/12345` where `example` is the App's URI scheme and `home/cupertino/12345` routes to unique content in the App
- * | "iosAppStoreId" | iTunes App Store ID for your iOS App
+ * | "iosAppId" | iTunes App Store ID for your iOS App
  * | "iosURL" | A custom scheme for your iOS App such as: `example/home/cupertino/12345`
  * | "data" | Any additional deep link data that you would like to pass to your App
  *
  * Resultant Firebase App Indexing tags will have the following format:
  *```
  * <link rel="alternate" href="android-app://{androidPackageName}/{androidURL}?{branch_tracking_params_and_additional_deep_link_data}"/>
- * <link rel="alternate" href="ios-app://{iosAppStoreId}/{iosURL}?{branch_tracking_params_and_additional_deep_link_data}"/>
+ * <link rel="alternate" href="ios-app://{iosAppId}/{iosURL}?{branch_tracking_params_and_additional_deep_link_data}"/>
  *```
  * Note: If optional parameters above are not specified, Branch will try to build Firebase App Indexing tags using your page's App Links tags.
  * Alternatively, if optional parameters are specified but Firebase App Indexing tags already exist on your webpage then Branch tracking params will be appended to the end of these tags and ignore what is passed into `Branch.autoAppIndex()`.
