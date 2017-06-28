fd0df29b04fc9804d4a9297a67dc1278593b65cd
Rubin Singh <rubinsingh@gmail.com>
2017-06-27 17:06:13 -0700

    passes link_click_id to v1/branchview

--- src/branch_view.js
+++ src/branch_view.js
@@ -153,7 +153,7 @@ function isJourneyDismissed(branchViewData, branch) {
 }
 
 // builds an object that contains data from setBranchViewData() call, hosted deep link data and language data
-function compileRequestData(branch) {
+function compileRequestData(branch, linkClickId) {
 		var requestData = branch._branchViewData || {};
 
 		if (!requestData['data']) {
@@ -162,6 +162,9 @@ function compileRequestData(branch) {
 
 		requestData['data'] = utils.merge(utils.scrapeHostedDeepLinkData(), requestData['data']);
 		requestData['data'] = utils.merge(utils.whiteListJourneysLanguageData(session.get(branch._storage) || {}), requestData['data']);
+		if (linkClickId) { // ignore passing linkClickId to v1/branchview if its null
+			requestData['data'] = utils.merge({'link_click_id': linkClickId}, requestData['data']);
+		}
 		return requestData;
 }
 
@@ -202,7 +205,15 @@ branch_view.initJourney = function(branch_key, data, eventData, options, branch)
 	if (branchViewData && !hideJourney && !no_journeys) {
 		journeys_utils.branchViewId = branchViewData.id;
 		branch['renderQueue'](function() {
-			requestData = compileRequestData(branch);
+			var linkClickId = null;
+			var referringLink = branch._referringLink();
+			var makeNewLink = options && options['make_new_link'] || false;
+			if (referringLink && !makeNewLink) {
+				linkClickId = referringLink.substring(
+					referringLink.lastIndexOf('/') + 1, referringLink.length
+				);
+			}
+			requestData = compileRequestData(branch, linkClickId);
 			branch_view.handleBranchViewData(branch._server, branchViewData, requestData, branch._storage, data['has_app'], testFlag, branch);
 		});
 	}
