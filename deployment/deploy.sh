fb7dca308786bb5819808dee7d90a5a01f5a5cff
Javeria Khan <jkhan@branch.io>
2017-08-10 23:42:56 -0700

    Final changes [ci skip]

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -80,13 +80,13 @@ if [ "$CIRCLE_BRANCH" == 'production' ]; then
   aws configure set preview.cloudfront true
   aws cloudfront create-invalidation --distribution-id E10P37NG0GMER --paths /*.min.js
 
-elif [ "$CIRCLE_BRANCH" == 'test-deploy' ]; then
+elif [ "$CIRCLE_BRANCH" == 'master' ]; then
   # Backup S3 bucket
   echo "Pushing to S3: branch-builds"
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-$VERSION.min.js --acl public-read
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-latest.min.js --acl public-read
   aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/websdk/branch-v2.0.0.min.js --acl public-read
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.js.gz s3://branch-builds/websdk/branch.js --acl public-read
+  aws s3 cp --content-type="text/javascript" dist/build.js s3://branch-builds/websdk/branch.js --acl public-read
   aws s3 cp example.html s3://branch-builds/websdk/example.html --acl public-read
 
 else
