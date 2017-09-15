18be2d2e22b7b1c1a4b01268b35937c3f03cf5ff
Rubin Singh <rubinsingh@gmail.com>
2017-09-15 14:57:38 -0700

    remove xmlns attribute from svg tag

--- src/4_banner_html.js
+++ src/4_banner_html.js
@@ -56,7 +56,7 @@ banner_html.banner = function(options, action) {
 													'</foreignObject>' +
 													'</span>' :
 												'<span class="full">' +
-													'<svg fill="#555555" height="12" viewBox="3 2 20 19" width="12" xmlns="http://www.w3.org/2000/svg">' +
+													'<svg fill="#555555" height="12" viewBox="3 2 20 19" width="12">' +
 													'<path d="M0 0h24v24H0z" fill="none"/>' +
 													'<path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>' +
 													'<path d="M0 0h24v24H0z" fill="none"/>' +
