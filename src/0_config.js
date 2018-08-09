c45e92efd7e853ee6c5c70fd541828117eef172c
Hemanth S A <hemanth@branch.io>
2018-08-08 17:39:14 -0700

    Revert config changes

--- src/0_config.js
+++ src/0_config.js
@@ -5,5 +5,5 @@ goog.provide('config');
 
 config.app_service_endpoint = 'https://app.link';
 config.link_service_endpoint = 'https://bnc.lt';
-config.api_endpoint = 'http://192.168.192.140:3002';
+config.api_endpoint = 'https://api.branch.io';
 config.version = '2.41.0';
