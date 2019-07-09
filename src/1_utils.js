0ed2caeaa3cadf9a6af8461f05ad5d06ef5ab160
Aaron Lopez <a.aaron.lopez@gmail.com>
2019-07-09 13:28:59 -0700

    Use operator

--- src/1_utils.js
+++ src/1_utils.js
@@ -1043,12 +1043,7 @@ utils.getInitialReferrer = function(referringLink) {
 };
 
 utils.getCurrentUrl = function() {
-	if (utils.isIframeAndFromSameOrigin()) {
-		return window.top.location.href || "";
-	}
-	else {
-		return window.location.href || "";
-	}
+	return utils.isIframeAndFromSameOrigin() ? window.top.location.href || "" : window.top.href || "";
 };
 
 // Required for logEvent()'s custom_data object - values must be converted to string
