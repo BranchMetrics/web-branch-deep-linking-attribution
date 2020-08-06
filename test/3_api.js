fc170e0af538bb761a606b1eb53c7d70b4022222
Jimmy Dee <jimmy.dee@branch.io>
2020-08-06 11:04:49 -0700

    Finish off first test

--- test/3_api.js
+++ test/3_api.js
@@ -1459,18 +1459,28 @@ describe('Server', function() {
 		it('receives all relevant fields from an XHR request if present', function(done) {
 			storage['set']('use_jsonp', false);
 
+			var params = testUtils.params({
+				"link_identifier": "1111111111"
+			});
 			server.onAPIResponse = function(url, method, requestBody, error, status, responseBody) {
-				assert.strictEqual(url, resources.open.destination + resources.open.endpoint);
-				assert.strictEqual(method, resources.open.method);
-				assert.strictEqual(status, 200);
-				done();
+				try {
+					assert.strictEqual(url, resources.open.destination + resources.open.endpoint);
+					assert.strictEqual(method, resources.open.method);
+					// Use regexp to avoid details of different browsers.
+					assert.ok(requestBody.match(/link_identifier=1111111111/));
+					assert.strictEqual(error, null);
+					assert.strictEqual(status, 200);
+					assert.deepEqual(responseBody, { 'session_id': 123 }, 'correct response');
+					done();
+				}
+				catch (error) {
+					done(error);
+				}
 			};
 
 			server.request(
 				resources.open,
-				testUtils.params({
-					"link_identifier": "1111111111"
-				}),
+				params,
 				storage,
 				function(err) {}
 			);
