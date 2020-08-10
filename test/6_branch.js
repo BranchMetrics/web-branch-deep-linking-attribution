f4ca6e49832bdb0b9243ecb470b0438eb1429ef6
Jimmy Dee <jimmy.dee@branch.io>
2020-08-10 15:10:23 -0700

    Restore Safari 11+ behavior

--- test/6_branch.js
+++ test/6_branch.js
@@ -469,10 +469,11 @@ describe('Branch', function() {
 			}
 		});
 
-		it('should not call _r in a WebKit browser', function(done) {
-			var originalWebKitURL = window.webkitURL;
-			if (!window.webkitURL) {
-				window.webkitURL = 'https://example.com';
+		it('should not call _r if userAgent is safari 11 or greater',	function(done) {
+			var safari11Ua = 'Mozilla/5.0 (iPod touch; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.28 (KHTML, like Gecko) Version/11.0 Mobile/15A5318g Safari/604.1';
+			setUserAgent(safari11Ua);
+			if (navigator.userAgent !== safari11Ua) {
+				return done();
 			}
 
 			var branch = initBranch(false);
@@ -511,19 +512,14 @@ describe('Branch', function() {
 				},
 				'Request to open params correct'
 			);
-
-			if (originalWebKitURL !== undefined) {
-				window.webkitURL = originalWebKitURL;
-			}
-			else {
-				delete window.webkitURL;
-			}
 		});
 
-		it('should not call _r if session present but no link_identifier and WebKit browser', function(done) {
-			var originalWebKitURL = window.webkitURL;
-			if (!window.webkitURL) {
-				window.webkitURL = 'https://example.com';
+		it('should not call _r if session present but no link_identifier and safari 11 or greater',
+			function(done) {
+			var safari11Ua = 'Mozilla/5.0 (iPod touch; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.28 (KHTML, like Gecko) Version/11.0 Mobile/15A5318g Safari/604.1';
+			setUserAgent(safari11Ua);
+			if (navigator.userAgent !== safari11Ua) {
+				return done();
 			}
 
 			var branch = initBranch(false);
@@ -555,13 +551,6 @@ describe('Branch', function() {
 				'/v1/has-app',
 				'Second request should be sent to /v1/has-app'
 			);
-
-			if (originalWebKitURL !== undefined) {
-				window.webkitURL = originalWebKitURL;
-			}
-			else {
-				delete window.webkitURL;
-			}
 		});
 
 	});
