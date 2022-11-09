86f27f07e9883acb103c763a83df5d8577933793
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2022-11-09 00:40:48 -0800

    linting changes

--- test/web-config.js
+++ test/web-config.js
@@ -2,7 +2,7 @@
  * Just a couple of variables that shouldn't change very often...
  */
 
- config.app_service_endpoint = 'https://app.link';
- config.link_service_endpoint = 'https://bnc.lt';
- config.api_endpoint = 'https://api.branch.io';
- config.version = '2.69.0'; 
+config.app_service_endpoint = 'https://app.link';
+config.link_service_endpoint = 'https://bnc.lt';
+config.api_endpoint = 'https://api.branch.io';
+config.version = '2.69.0';
