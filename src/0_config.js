ff2897f686f9b3b99aaf0ba1cf61f0bb56856877
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2022-11-09 00:49:09 -0800

    linting changes

--- src/0_config.js
+++ src/0_config.js
@@ -1,9 +1,9 @@
 /**
  * Just a couple of variables that shouldn't change very often...
  */
- goog.provide('config');
+goog.provide('config');
 
- config.app_service_endpoint = 'https://app.link';
- config.link_service_endpoint = 'https://bnc.lt';
- config.api_endpoint = 'https://api2.branch.io';
- config.version = '2.69.0';
+config.app_service_endpoint = 'https://app.link';
+config.link_service_endpoint = 'https://bnc.lt';
+config.api_endpoint = 'https://api2.branch.io';
+config.version = '2.69.0';
