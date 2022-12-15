7115df33f7a4107adaf7d1348161711eb393f99f
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2022-12-15 10:44:31 -0800

    update to latest version

--- test/web-config.js
+++ test/web-config.js
@@ -5,4 +5,4 @@
 config.app_service_endpoint = 'https://app.link';
 config.link_service_endpoint = 'https://bnc.lt';
 config.api_endpoint = 'https://api.branch.io';
-config.version = '2.70.0';
+config.version = '2.71.0';
