b8e1a7c59bcdc37511fbe11a9c4e09dc0bd21388
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-04-11 14:35:56 -0700

    [SDK-1789] codecov GHA integration (#890)
    * codecov GHA integration
    
    * Fix typo

---
+++ deployment/deploy-qa.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+# Deploy to QA
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+#--------------------------------------------------------------------------------------------
+# Main branch (QA) Deploy
+#--------------------------------------------------------------------------------------------
+
+  echo -en "${GREEN}QA Release...${NC}\n"
+
+  echo -en "${GREEN}make release ...${NC}\n"
+  make release
+
+  echo -en "${GREEN}Pushing to builds ...${NC}\n"
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-builds/web-sdk/branch-latest.min.js
+  aws s3 cp --content-type="text/javascript" dist/build.js s3://branch-builds/web-sdk/branch.js
+  aws s3 cp example.html s3://branch-builds/web-sdk/example.html
+
+  echo -en "${GREEN}Pushing to CDN ...${NC}\n"
+  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-cdn/branch-staging-latest.min.js --cache-control "max-age=300"
+  aws s3 cp dist/example.html s3://branch-cdn/example-staging.html
+  
+  echo -en "Invalidating cloudfront distribution for staging ...\n"
+  aws configure set preview.cloudfront true
+  aws cloudfront create-invalidation --distribution-id E10P37NG0GMER --paths /branch-staging-latest.min.js /example-staging
+# Exit prompts
+echo -en "${GREEN}Done deploy QA script ...${NC}\n"
