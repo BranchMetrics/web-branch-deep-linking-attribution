fb21645bbdb1e2831c7c1dba6512cd0d3cb972d5
Jagadeesh Karicherla <jagadeesh.karicherla@branch.io>
2023-06-14 13:33:07 -0700

    test(core): add config unit tests
    add config unit tests

--- test/0_config.js
+++ test/0_config.js
@@ -3,36 +3,36 @@
 goog.require('config');
 
 
-describe('utils', function() {
+describe('config', function() {
 	var assert = testUtils.unplanned();
 	describe('app_service_endpoint', function() {
 		it('app_service_endpoint should be defined', function() {
 			assert.isDefined(config.app_service_endpoint, 'app_service_endpoint is un-defined');
 		});
-        it('app_service_endpoint value is correct', function() {
+		it('app_service_endpoint value is correct', function() {
 			assert.isDefined(config.app_service_endpoint, 'https://app.link', 'app_service_endpoint is incorrect');
 		});
 	});
 
-    describe('link_service_endpoint', function() {
+	describe('link_service_endpoint', function() {
 		it('link_service_endpoint should be defined', function() {
 			assert.isDefined(config.link_service_endpoint, 'link_service_endpoint is un-defined');
 		});
-        it('link_service_endpoint value is correct', function() {
+		it('link_service_endpoint value is correct', function() {
 			assert.isDefined(config.link_service_endpoint, 'https://bnc.lt', 'link_service_endpoint is incorrect');
 		});
 	});
 
-    describe('api_endpoint', function() {
+	describe('api_endpoint', function() {
 		it('api_endpoint should be defined', function() {
 			assert.isDefined(config.api_endpoint, 'api_endpoint is un-defined');
 		});
-        it('api_endpoint value is correct', function() {
+		it('api_endpoint value is correct', function() {
 			assert.isDefined(config.api_endpoint, 'https://api2.branch.io', 'api_endpoint is incorrect');
 		});
 	});
 
-    describe('version', function() {
+	describe('version', function() {
 		it('version should be defined', function() {
 			assert.isDefined(config.version, 'version is un-defined');
 		});
