b48e7a4dff85a3e26193a00c85059a2f682be86f
Jagadeesh Branch <102190347+JagadeeshKaricherla-branch@users.noreply.github.com>
2023-06-28 09:54:43 -0700

    ci(core): add new npm scripts for clean, coverage (#905)
    * ci(core): add new npm scripts for clean, coverage
    
    add new npm run script for clean, coverage, build
    
     moved google closure to dev dependencies
    
    updated sinon package to latest
    
    * ci(core): add GHA to publish unit test results
    
    add GHA to publish unit test results
    
    * ci(core): publish unit test results as PR comment
    
    publish unit test results as PR comment
    
    * chore(ci): add codecov to gate-keep.yml
    
    * chore(other): add lcov format for nyc reporting
    
    add lcov format for nyc reporting
    
    * test(core): add config unit tests
    
    add config unit tests
    
    * test(core): add config unit tests
    
    add config unit tests
    
    * test(core): modify test/branch-deps to include config unit tests
    
    modify test/branch-deps to include config unit tests

---
+++ test/0_config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+goog.require('config');
+
+
+describe('config', function() {
+	var assert = testUtils.unplanned();
+	describe('app_service_endpoint', function() {
+		it('app_service_endpoint should be defined', function() {
+			assert.isDefined(config.app_service_endpoint, 'app_service_endpoint is un-defined');
+		});
+		it('app_service_endpoint value is correct', function() {
+			assert.isDefined(config.app_service_endpoint, 'https://app.link', 'app_service_endpoint is incorrect');
+		});
+	});
+
+	describe('link_service_endpoint', function() {
+		it('link_service_endpoint should be defined', function() {
+			assert.isDefined(config.link_service_endpoint, 'link_service_endpoint is un-defined');
+		});
+		it('link_service_endpoint value is correct', function() {
+			assert.isDefined(config.link_service_endpoint, 'https://bnc.lt', 'link_service_endpoint is incorrect');
+		});
+	});
+
+	describe('api_endpoint', function() {
+		it('api_endpoint should be defined', function() {
+			assert.isDefined(config.api_endpoint, 'api_endpoint is un-defined');
+		});
+		it('api_endpoint value is correct', function() {
+			assert.isDefined(config.api_endpoint, 'https://api2.branch.io', 'api_endpoint is incorrect');
+		});
+	});
+
+	describe('version', function() {
+		it('version should be defined', function() {
+			assert.isDefined(config.version, 'version is un-defined');
+		});
+	});
+});
