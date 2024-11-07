a7b45bf9a692d217bc5248708ba5b340dab88ab2
Brice Redmond <brice.redmond@branch.io>
2024-11-07 15:46:45 -0800

    [patch] fix release.sh
    2 problems:
    - the s3 publishes should not have had acl (i got this from what was there before)
    - the npm publish needs a valid token

--- deployment/release-s3.sh
+++ deployment/release-s3.sh
@@ -12,15 +12,15 @@ make release
 
 ./deployment/build-example-html.sh "key_live_hcnegAumkH7Kv18M8AOHhfgiohpXq5tB" "https://api2.branch.io" "https://cdn.branch.io/branch-latest.min.js"
 aws s3 cp example.html s3://branch-builds/example.html
+aws s3 cp example.html s3://branch-cdn/example.html
 
-aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-cdn/branch-$VERSION.min.js --acl public-read
-aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-cdn/branch-latest.min.js --acl public-read
+aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-cdn/branch-$VERSION.min.js --cache-control "max-age=300"
+aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://branch-cdn/branch-latest.min.js --cache-control "max-age=300"
 
 echo -en "Invalidating cloudfront distribution...\n"
 aws configure set preview.cloudfront true
 aws cloudfront create-invalidation --distribution-id E10P37NG0GMER --paths /branch-latest.min.js
 
-npm publish
 
 echo "Post-release sanity checks."
 read -p "Can you visit https://cdn.branch.io/branch-$VERSION.min.js ?" -n 1 -r
